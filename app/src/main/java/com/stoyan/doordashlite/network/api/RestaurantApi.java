671968064bc18d8a1d9d0ce580ef83cde69630f4
StoyanD <s.dimitrov19@gmail.com>
2018-01-30 15:29:14 -0800

    -adding list item xml

--- app/src/main/java/com/stoyan/doordashlite/network/api/RestaurantApi.java
+++ app/src/main/java/com/stoyan/doordashlite/network/api/RestaurantApi.java
@@ -1,8 +1,20 @@
 package com.stoyan.doordashlite.network.api;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by stoyan on 1/30/18.
  */
 
 public class RestaurantApi {
+
+    public int id;
+    public String name;
+    public String description;
+    @SerializedName("cover_img_url")
+    public String coverImgUrl;
+    public String status;
+    @SerializedName("delivery_fee")
+    public String deliveryFee;
+
 }
